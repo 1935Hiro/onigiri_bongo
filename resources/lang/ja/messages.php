5b66eb0a74e550d6518e6c28520e273b54942f34
1935Hiro <espresso.spacewood@gmail.com>
2022-05-01 05:22:49 +0000

    second commit0501

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,8 @@
+<? php
+
+return [
+        'login'=>'ログイン',
+        'E-mail address'=>'メールアドレス',
+        'password'=>'パスワード',
+        'Remenber me'=>'ログイン情報を記憶する',
+    ]
---
+++ resources/views/admin/mypage/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+
+
+@section('title', 'プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
+<!--<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>My page</title>
+    </head>
+    <body>
+        <h1>プロフィール</h1>
+    </body>
+</html>-->
